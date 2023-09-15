f7c644d61aab8bc0ad1d29f9e585ef2515c3adb6
Sandeep Rai <_chamling@Sandeeps-MacBook-Pro.local>
2023-09-15 22:13:57 +1000

    plotting the diagram for the climate_q2.py

--- climate_q2.py
+++ climate_q2.py
@@ -6,27 +6,19 @@ df = pd.read_csv("climate.csv")
 df1 = df.head(10)
 print(df1)
 
-years = []
-co2 = []
-temp = []
-
-for i in range(len(df)):
-    if df['Years']:
-        years.append(df['Years'])
-    elif df['CO2']:
-        co2.append(df['CO2'])
-    else:
-        temp.append(df['Temperature'])
+years = list(df['Year'])
+co2 = list(df['CO2'])
+temp = list(df['Temperature'])
 
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'bo')
+plt.plot(years, co2, 'b--')
 plt.title("Climate Data")
 plt.ylabel("[CO2]")
 plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*')
+plt.plot(years, temp, '*--')
 plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)")
 plt.show()
